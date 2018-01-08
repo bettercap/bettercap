d052758efc6a2fb9a5ebbf094e755f66459fc757
evilsocket <evilsocket@gmail.com>
2018-01-08 09:50:35 +0100

    refact: renamed api.rest TLS files

--- modules/api_rest.go
+++ modules/api_rest.go
@@ -43,12 +43,12 @@ func NewRestAPI(s *session.Session) *RestAPI {
 		"API authentication username."))
 
 	api.AddParam(session.NewStringParameter("api.rest.certificate",
-		"~/.bettercap-ng.certificate.pem",
+		"~/.bettercap-ng.api.rest.certificate.pem",
 		"",
 		"API TLS certificate."))
 
 	api.AddParam(session.NewStringParameter("api.rest.key",
-		"~/.bettercap-ng.key.pem",
+		"~/.bettercap-ng.api.rest.key.pem",
 		"",
 		"API TLS key"))
 
