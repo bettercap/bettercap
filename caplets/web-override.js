74867aaae4eb606469676e19472e67f2b1564a9a
evilsocket <evilsocket@gmail.com>
2018-01-09 22:52:56 +0100

    new: web-override caplet and proxy module

---
+++ caplets/web-override.js
@@ -0,0 +1,10 @@
+// Called before every request is executed, just override the response with 
+// our own html web page.
+function onRequest(req, res) {
+    res.Status      = 200;
+    res.ContentType = "text/html";
+    res.Headers     = "Connection: close";
+    res.Body        =  readFile("caplets/web-override.html");
+
+    res.Updated();
+}
