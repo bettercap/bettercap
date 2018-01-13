52623ab277833f6eabb3a91af8402c5d573b3010
evilsocket <evilsocket@gmail.com>
2018-01-13 14:44:43 +0100

    balls

--- net/arp_parser_darwin.go
+++ net/arp_parser_darwin.go
@@ -2,5 +2,5 @@ package net
 
 import "regexp"
 
-var ArpTableParser = regexp.MustCompile("^[^\\d\\.]+([\\d\\.]+).+\\s+([a-f0-9:]{17})\\s+on\\s+(.+)\\s+.+$")
+var ArpTableParser = regexp.MustCompile("^[^\\d\\.]+([\\d\\.]+).+\\s+([a-f0-9:]{17})\\s+on\\s+([^\\s]+)\\s+.+$")
 var ArpTableTokens = 4
