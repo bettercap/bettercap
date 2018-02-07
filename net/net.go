017c144b20c6570f97b6e3f2b5bf5a08fc38d89f
evilsocket <evilsocket@gmail.com>
2018-02-07 20:29:22 +0100

    balls

--- net/net.go
+++ net/net.go
@@ -16,7 +16,6 @@ import (
 const MonitorModeAddress = "0.0.0.0"
 
 func areTheSame(iface net.Interface, piface pcap.Interface) bool {
-	fmt.Printf("%+v\n", piface)
 	if addrs, err := iface.Addrs(); err == nil {
 		for _, ia := range addrs {
 			for _, ib := range piface.Addresses {
@@ -46,8 +45,6 @@ func getInterfaceName(iface net.Interface) string {
 			return dev.Name
 		}
 	}
-
-	fmt.Println("NOOPE")
 
 	return iface.Name
 }
