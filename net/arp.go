318932453ca5dc051e62bf2f0411f7c8ce51bf29
evilsocket <evilsocket@gmail.com>
2018-02-07 16:40:05 +0100

    balls

--- net/arp.go
+++ net/arp.go
@@ -2,7 +2,10 @@ package net
 
 import (
 	"fmt"
+	"strings"
 	"sync"
+
+	"github.com/evilsocket/bettercap-ng/core"
 )
 
 type ArpTable map[string]string
@@ -12,6 +15,38 @@ var (
 	arpLock      = &sync.Mutex{}
 	arpTable     = make(ArpTable)
 )
+
+func ArpUpdate(iface string) (ArpTable, error) {
+	arpLock.Lock()
+	defer arpLock.Unlock()
+
+	// Signal we parsed the ARP table at least once.
+	arpWasParsed = true
+
+	// Run "arp -an" (darwin) or "ip neigh" (linux) and parse the output
+	output, err := core.Exec(ArpCmd, ArpCmdOpts)
+	if err != nil {
+		return arpTable, err
+	}
+
+	newTable := make(ArpTable)
+	for _, line := range strings.Split(output, "\n") {
+		m := ArpTableParser.FindStringSubmatch(line)
+		if len(m) == ArpTableTokens {
+			address := m[ArpTableTokenIndex[0]]
+			mac := m[ArpTableTokenIndex[1]]
+			ifname := m[ArpTableTokenIndex[2]]
+
+			if ifname == iface {
+				newTable[address] = mac
+			}
+		}
+	}
+
+	arpTable = newTable
+
+	return arpTable, nil
+}
 
 func ArpLookup(iface string, address string, refresh bool) (string, error) {
 	// Refresh ARP table if first run or if a force refresh has been instructed.
