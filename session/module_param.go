3a29ca1fbbe7bc9e5fc0c75110e5811e3bb6ad63
evilsocket <evilsocket@gmail.com>
2018-02-08 08:29:37 +0100

    fix: using a clearer error message when a module parameter is not valid

--- session/module_param.go
+++ session/module_param.go
@@ -59,7 +59,7 @@ func NewIntParameter(name string, def_value string, desc string) *ModuleParam {
 func (p ModuleParam) Validate(value string) (error, interface{}) {
 	if p.Validator != nil {
 		if p.Validator.MatchString(value) == false {
-			return fmt.Errorf("Parameter value '%s' does not match validator '%s'.", value, p.Validator.String()), nil
+			return fmt.Errorf("%s: value '%s' does not validate '%s'.", core.Bold(p.Name), value, p.Validator.String()), nil
 		}
 	}
 
