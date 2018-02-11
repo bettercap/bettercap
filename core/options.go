ae81bb86068bfd801ea7cb8e092bc1be4e77044d
evilsocket <evilsocket@gmail.com>
2018-02-11 19:32:44 +0100

    misc: small fix or general refactoring i did not bother commenting

--- core/options.go
+++ core/options.go
@@ -21,8 +21,8 @@ func ParseOptions() (Options, error) {
 		Silent:        flag.Bool("silent", false, "Suppress all logs which are not errors."),
 		NoHistory:     flag.Bool("no-history", false, "Disable interactive session history file."),
 		Commands:      flag.String("eval", "", "Run one or more commands separated by ; in the interactive session, used to set variables via command line."),
-		CpuProfile:    flag.String("cpuprofile", "", "Write cpu profile `file`."),
-		MemProfile:    flag.String("memprofile", "", "Write memory profile to `file`."),
+		CpuProfile:    flag.String("cpu-profile", "", "Write cpu profile `file`."),
+		MemProfile:    flag.String("mem-profile", "", "Write memory profile to `file`."),
 	}
 
 	flag.Parse()
