f4baac69a551c72aca59579d0c4cb9960289080d
evilsocket <evilsocket@gmail.com>
2018-02-11 20:33:06 +0100

    misc: small fix or general refactoring i did not bother commenting

--- packets/serialize.go
+++ packets/serialize.go
@@ -4,17 +4,15 @@ import (
 	"github.com/google/gopacket"
 )
 
-func Serialize(layers ...gopacket.SerializableLayer) (error, []byte) {
-	// Set up buffer and options for serialization.
-	buf := gopacket.NewSerializeBuffer()
-	opts := gopacket.SerializeOptions{
-		FixLengths:       true,
-		ComputeChecksums: true,
-	}
+var SerializationOptions = gopacket.SerializeOptions{
+	FixLengths:       true,
+	ComputeChecksums: true,
+}
 
-	if err := gopacket.SerializeLayers(buf, opts, layers...); err != nil {
+func Serialize(layers ...gopacket.SerializableLayer) (error, []byte) {
+	buf := gopacket.NewSerializeBuffer()
+	if err := gopacket.SerializeLayers(buf, SerializationOptions, layers...); err != nil {
 		return err, nil
 	}
-
 	return nil, buf.Bytes()
 }
