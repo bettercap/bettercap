b63c20b757e1936b97fde6c403c5415a6178bfed
evilsocket <evilsocket@gmail.com>
2018-03-12 17:13:28 +0100

    misc: small fix or general refactoring i did not bother commenting

--- build.sh
+++ build.sh
@@ -13,6 +13,16 @@ host_dep() {
     ping -c 1 $HOST > /dev/null || { echo "@ Virtual machine host $HOST not visible !"; exit 1; }
 }
 
+create_exe_archive() {
+    bin_dep 'zip'
+
+    OUTPUT=$1
+
+    echo "@ Creating archive $OUTPUT ..."
+    zip -j "$OUTPUT" bettercap.exe ../README.md ../LICENSE.md > /dev/null
+    rm -rf bettercap bettercap.exe
+}
+
 create_archive() {
     bin_dep 'zip'
 
@@ -172,19 +182,17 @@ mkdir $BUILD_FOLDER
 cd $BUILD_FOLDER
 
 
-build_linux_amd64 # && create_archive bettercap_linux_amd64_$VERSION.zip
-# build_linux_arm7_static && create_archive bettercap_linux_arm7_$VERSION.zip
-# build_linux_arm7hf_static && create_archive bettercap_linux_arm7hf_$VERSION.zip
-# build_linux_mips_static && create_archive bettercap_linux_mips_$VERSION.zip
-# build_linux_mipsle_static && create_archive bettercap_linux_mipsle_$VERSION.zip
-# build_linux_mips64_static && create_archive bettercap_linux_mips64_$VERSION.zip
-# build_linux_mips64le_static && create_archive bettercap_linux_mips64le_$VERSION.zip
-# 
-# # these are still not static :(
-# build_macos_amd64 && create_archive bettercap_macos_amd64_$VERSION.zip
-# build_android_arm && create_archive bettercap_android_arm_$VERSION.zip
-# build_windows_amd64 && create_archive bettercap_windows_amd64_$VERSION.zip
-# sha256sum * > checksums.txt
+build_linux_amd64 && create_archive bettercap_linux_amd64_$VERSION.zip
+build_macos_amd64 && create_archive bettercap_macos_amd64_$VERSION.zip
+build_android_arm && create_archive bettercap_android_arm_$VERSION.zip
+build_windows_amd64 && create_exe_archive bettercap_windows_amd64_$VERSION.zip
+build_linux_arm7_static && create_archive bettercap_linux_arm7_$VERSION.zip
+build_linux_arm7hf_static && create_archive bettercap_linux_arm7hf_$VERSION.zip
+build_linux_mips_static && create_archive bettercap_linux_mips_$VERSION.zip
+build_linux_mipsle_static && create_archive bettercap_linux_mipsle_$VERSION.zip
+build_linux_mips64_static && create_archive bettercap_linux_mips64_$VERSION.zip
+build_linux_mips64le_static && create_archive bettercap_linux_mips64le_$VERSION.zip
+sha256sum * > checksums.txt
 
 echo
 echo
