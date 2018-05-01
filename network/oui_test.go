29ec4edc633cc167b91413f99a876f744bf6375e
Kent Gruber <kgruber1@emich.edu>
2018-05-01 15:47:33 -0400

    add test for OuiLookup

--- network/oui_test.go
+++ network/oui_test.go
@@ -7,3 +7,12 @@ func TestOuiVar(t *testing.T) {
 		t.Error("unable to find any oui infromation")
 	}
 }
+
+func TestOuiLookup(t *testing.T) {
+	exampleMac := "e0:0c:7f:XX:XX:XX"
+	exp := "Nintendo Co."
+	got := OuiLookup(exampleMac)
+	if got != exp {
+		t.Fatalf("expected '%s', got '%s'", exp, got)
+	}
+}
