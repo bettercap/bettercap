74c99d5ea1ca3ee780677265da46ac7454190c61
Kent Gruber <kgruber1@emich.edu>
2018-05-01 22:37:03 -0400

    add test for Dot11Chan2Freq

--- network/wifi_test.go
+++ network/wifi_test.go
@@ -14,3 +14,12 @@ func TestDot11Freq2Chan(t *testing.T) {
 		t.Fatalf("expected '%v', got '%v'", exp, got)
 	}
 }
+
+func TestDot11Chan2Freq(t *testing.T) {
+	exampleChan := 13
+	exp := 2472
+	got := Dot11Chan2Freq(exampleChan)
+	if got != exp {
+		t.Fatalf("expected '%v', got '%v'", exp, got)
+	}
+}
