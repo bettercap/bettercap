7e21ec9a97f875bd75fa36c1964d86edb28a825b
Kent Gruber <kgruber1@emich.edu>
2018-05-01 22:36:45 -0400

    add test for Dot11Freq2Chan

--- network/wifi_test.go
+++ network/wifi_test.go
@@ -5,3 +5,12 @@ import "testing"
 func buildExampleWiFi() *WiFi {
 	return NewWiFi(buildExampleEndpoint(), func(ap *AccessPoint) {}, func(ap *AccessPoint) {})
 }
+
+func TestDot11Freq2Chan(t *testing.T) {
+	exampleFreq := 2472
+	exp := 13
+	got := Dot11Freq2Chan(exampleFreq)
+	if got != exp {
+		t.Fatalf("expected '%v', got '%v'", exp, got)
+	}
+}
