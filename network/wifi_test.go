00244349449314eacd537df97c0215040b549952
Kent Gruber <kgruber1@emich.edu>
2018-05-01 22:39:19 -0400

    add test for List

--- network/wifi_test.go
+++ network/wifi_test.go
@@ -68,3 +68,15 @@ func TestStations(t *testing.T) {
 		t.Fatalf("expected '%v', got '%v'", exp, got)
 	}
 }
+
+func TestWiFiList(t *testing.T) {
+	exampleWiFi := buildExampleWiFi()
+	exampleAP := NewAccessPoint("my_wifi", "ff:ff:ff:ff:ff:ff", 2472, int8(0))
+	exampleWiFi.aps["ff:ff:ff:ff:ff:f1"] = exampleAP
+	exampleWiFi.aps["ff:ff:ff:ff:ff:f2"] = exampleAP
+	exp := 2
+	got := len(exampleWiFi.List())
+	if got != exp {
+		t.Fatalf("expected '%v', got '%v'", exp, got)
+	}
+}
