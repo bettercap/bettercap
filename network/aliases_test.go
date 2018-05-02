b91a6b51b35905d63fcea1072932ad9232694ac7
Kent Gruber <kgruber1@emich.edu>
2018-05-02 13:56:44 -0400

    add test for LoadAliases function

--- network/aliases_test.go
+++ network/aliases_test.go
@@ -5,3 +5,10 @@ import "testing"
 func buildExampleAlaises() *Aliases {
 	return &Aliases{}
 }
+
+func TestAliasesLoadAliases(t *testing.T) {
+	err, _ := LoadAliases()
+	if err != nil {
+		t.Error(err)
+	}
+}
