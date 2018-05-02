889df698d1413da054f42109a2ce5b5173c027c0
Kent Gruber <kgruber1@emich.edu>
2018-05-02 13:16:30 -0400

    add test for NewMeta function

--- network/meta_test.go
+++ network/meta_test.go
@@ -8,3 +8,11 @@ import (
 func buildExampleMeta() *Meta {
 	return NewMeta()
 }
+
+func TestNewMeta(t *testing.T) {
+	exp := len(Meta{}.m)
+	got := len(NewMeta().m)
+	if got != exp {
+		t.Fatalf("expected '%v', got '%v'", exp, got)
+	}
+}
