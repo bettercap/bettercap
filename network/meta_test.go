ec3227a16fda7de7bf7498ad16df2ac5ceb81082
Kent Gruber <kgruber1@emich.edu>
2018-05-02 13:16:49 -0400

    add test for MarshalJSON function

--- network/meta_test.go
+++ network/meta_test.go
@@ -16,3 +16,10 @@ func TestNewMeta(t *testing.T) {
 		t.Fatalf("expected '%v', got '%v'", exp, got)
 	}
 }
+
+func TestMetaMarshalJSON(t *testing.T) {
+	_, err := buildExampleMeta().MarshalJSON()
+	if err != nil {
+		t.Error("unable to marshal JSON from meta struct")
+	}
+}
