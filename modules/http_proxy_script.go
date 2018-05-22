a3ef4e66f0758d1958aed71f99c6bd574fbeebe5
evilsocket <evilsocket@gmail.com>
2018-05-22 12:46:46 +0200

    misc: small fix or general refactoring i did not bother commenting

--- modules/http_proxy_script.go
+++ modules/http_proxy_script.go
@@ -33,7 +33,7 @@ func LoadHttpProxyScriptSource(path, source string, sess *session.Session) (err 
 	if s.hasCallback("onRequest") {
 		s.onRequestScript, err = s.VM.Compile("", "onRequest(req, res)")
 		if err != nil {
-			log.Error( "Error while compiling onRequest callback: %s", "\nTraceback:\n  " + err.(*otto.Error).String() )
+			log.Error("Error while compiling onRequest callback: %s", "\nTraceback:\n  "+err.(*otto.Error).String())
 			return
 		}
 	}
@@ -41,7 +41,7 @@ func LoadHttpProxyScriptSource(path, source string, sess *session.Session) (err 
 	if s.hasCallback("onResponse") {
 		s.onResponseScript, err = s.VM.Compile("", "onResponse(req, res)")
 		if err != nil {
-			log.Error( "Error while compiling onResponse callback: %s", "\nTraceback:\n  " + err.(*otto.Error).String() )
+			log.Error("Error while compiling onResponse callback: %s", "\nTraceback:\n  "+err.(*otto.Error).String())
 			return
 		}
 	}
@@ -49,7 +49,7 @@ func LoadHttpProxyScriptSource(path, source string, sess *session.Session) (err 
 	if s.hasCallback("onCommand") {
 		s.onCommandScript, err = s.VM.Compile("", "onCommand(cmd)")
 		if err != nil {
-			log.Error( "Error while compiling onCommand callback: %s", "\nTraceback:\n  " + err.(*otto.Error).String() )
+			log.Error("Error while compiling onCommand callback: %s", "\nTraceback:\n  "+err.(*otto.Error).String())
 			return
 		}
 	}
@@ -71,13 +71,13 @@ func LoadHttpProxyScript(path string, sess *session.Session) (err error, s *Http
 func (s *HttpProxyScript) doRequestDefines(req *http.Request) (err error, jsreq *JSRequest, jsres *JSResponse) {
 	jsreq = NewJSRequest(req)
 	if err = s.VM.Set("req", jsreq); err != nil {
-		log.Error( "Error while defining request: %s", "\nTraceback:\n  " + err.(*otto.Error).String() )
+		log.Error("Error while defining request: %s", "\nTraceback:\n  "+err.(*otto.Error).String())
 		return
 	}
 
 	jsres = NewJSResponse(nil)
 	if err = s.VM.Set("res", jsres); err != nil {
-		log.Error( "Error while defining response: %s", "\nTraceback:\n  " + err.(*otto.Error).String() )
+		log.Error("Error while defining response: %s", "\nTraceback:\n  "+err.(*otto.Error).String())
 		return
 	}
 	return
@@ -86,13 +86,13 @@ func (s *HttpProxyScript) doRequestDefines(req *http.Request) (err error, jsreq 
 func (s *HttpProxyScript) doResponseDefines(res *http.Response) (err error, jsreq *JSRequest, jsres *JSResponse) {
 	jsreq = NewJSRequest(res.Request)
 	if err = s.VM.Set("req", jsreq); err != nil {
-		log.Error( "Error while defining request: %s", "\nTraceback:\n  " + err.(*otto.Error).String() )
+		log.Error("Error while defining request: %s", "\nTraceback:\n  "+err.(*otto.Error).String())
 		return
 	}
 
 	jsres = NewJSResponse(res)
 	if err = s.VM.Set("res", jsres); err != nil {
-		log.Error( "Error while defining response: %s", "\nTraceback:\n  " + err.(*otto.Error).String() )
+		log.Error("Error while defining response: %s", "\nTraceback:\n  "+err.(*otto.Error).String())
 		return
 	}
 
@@ -101,7 +101,7 @@ func (s *HttpProxyScript) doResponseDefines(res *http.Response) (err error, jsre
 
 func (s *HttpProxyScript) doCommandDefines(cmd string) (err error) {
 	if err = s.VM.Set("cmd", cmd); err != nil {
-		log.Error( "Error while defining cmd: %s", "\nTraceback:\n  " + err.(*otto.Error).String() )
+		log.Error("Error while defining cmd: %s", "\nTraceback:\n  "+err.(*otto.Error).String())
 	}
 	return
 }
@@ -114,12 +114,12 @@ func (s *HttpProxyScript) OnRequest(original *http.Request) (jsreq *JSRequest, j
 		defer s.Unlock()
 
 		if err, jsreq, jsres = s.doRequestDefines(original); err != nil {
-			log.Error( "Error while running bootstrap definitions: %s", "\nTraceback:\n  " + err.(*otto.Error).String() )
+			log.Error("Error while running bootstrap definitions: %s", "\nTraceback:\n  "+err.(*otto.Error).String())
 			return nil, nil
 		}
 
 		if _, err = s.VM.Run(s.onRequestScript); err != nil {
-			log.Error( "Error while executing onRequest callback: %s", "\nTraceback:\n  " + err.(*otto.Error).String() )
+			log.Error("Error while executing onRequest callback: %s", "\nTraceback:\n  "+err.(*otto.Error).String())
 			return nil, nil
 		}
 
@@ -143,12 +143,12 @@ func (s *HttpProxyScript) OnResponse(res *http.Response) (jsreq *JSRequest, jsre
 		defer s.Unlock()
 
 		if err, jsreq, jsres = s.doResponseDefines(res); err != nil {
-			log.Error( "Error while running bootstrap definitions: %s", "\nTraceback:\n  " + err.(*otto.Error).String() )
+			log.Error("Error while running bootstrap definitions: %s", "\nTraceback:\n  "+err.(*otto.Error).String())
 			return nil, nil
 		}
 
 		if _, err = s.VM.Run(s.onResponseScript); err != nil {
-			log.Error( "Error while executing onRequest callback: %s", "\nTraceback:\n  " + err.(*otto.Error).String() )
+			log.Error("Error while executing onRequest callback: %s", "\nTraceback:\n  "+err.(*otto.Error).String())
 			return nil, nil
 		}
 
@@ -167,12 +167,12 @@ func (s *HttpProxyScript) OnCommand(cmd string) bool {
 		defer s.Unlock()
 
 		if err := s.doCommandDefines(cmd); err != nil {
-			log.Error( "Error while running bootstrap onCommand definitions: %s", "\nTraceback:\n  " + err.(*otto.Error).String() )
+			log.Error("Error while running bootstrap onCommand definitions: %s", "\nTraceback:\n  "+err.(*otto.Error).String())
 			return false
 		}
 
 		if ret, err := s.VM.Run(s.onCommandScript); err != nil {
-			log.Error( "Error while executing onCommand callback: %s", "\nTraceback:\n  " + err.(*otto.Error).String() )
+			log.Error("Error while executing onCommand callback: %s", "\nTraceback:\n  "+err.(*otto.Error).String())
 			return false
 		} else if v, err := ret.ToBoolean(); err == nil {
 			return v
