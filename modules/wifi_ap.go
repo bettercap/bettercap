bettercap/bettercap

commit 0de6f3a76eb0155ed9971abc60974e4199361705

more lint driven refactoring

modules/wifi_ap.go
@@ -16,7 +16,6 @@ var errNoRecon = errors.New("Module wifi.ap requires module wifi.recon to be act
 
 func (w *WiFiModule) parseApConfig() (err error) {
 	var bssid string
-
 	if err, w.apConfig.SSID = w.StringParam("wifi.ap.ssid"); err != nil {
 		return
 	} else if err, bssid = w.StringParam("wifi.ap.bssid"); err != nil {
@@ -28,13 +27,12 @@ func (w *WiFiModule) parseApConfig() (err error) {
 	} else if err, w.apConfig.Encryption = w.BoolParam("wifi.ap.encryption"); err != nil {
 		return
 	}
-
 	return
 }
 
 func (w *WiFiModule) startAp() error {
 	// we need channel hopping and packet injection for this
-	if w.Running() == false {
+	if w.Running() {
 		return errNoRecon
 	} else if w.apRunning {
 		return session.ErrAlreadyStarted
@@ -47,7 +45,7 @@ func (w *WiFiModule) startAp() error {
 		}()
 
 		enc := core.Yellow("WPA2")
-		if w.apConfig.Encryption == false {
+		if !w.apConfig.Encryption {
 			enc = core.Green("Open")
 		}
 		log.Info("Sending beacons as SSID %s (%s) on channel %d (%s).",

network/wifi_ap.go
@@ -43,7 +43,7 @@ func (ap *AccessPoint) Get(bssid string) (*Station, bool) {
 	defer ap.Unlock()
 
 	bssid = NormalizeMac(bssid)
-	if s, found := ap.clients[bssid]; found == true {
+	if s, found := ap.clients[bssid]; found {
 		return s, true
 	}
 	return nil, false
@@ -55,7 +55,7 @@ func (ap *AccessPoint) AddClient(bssid string, frequency int, rssi int8) *Statio
 
 	bssid = NormalizeMac(bssid)
 
-	if s, found := ap.clients[bssid]; found == true {
+	if s, found := ap.clients[bssid]; found {
 		// update
 		s.Frequency = frequency
 		s.RSSI = rssi

firewall/firewall_darwin.go
@@ -122,7 +122,7 @@ func (f *PfFirewall) enable(enabled bool) {
 func (f PfFirewall) EnableRedirection(r *Redirection, enabled bool) error {
 	rule := f.generateRule(r)
 
-	if enabled == true {
+	if enabled {
 		fd, err := os.OpenFile(f.filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
 		if err != nil {
 			return err

firewall/firewall_windows.go
@@ -54,7 +54,7 @@ func (f WindowsFirewall) generateRule(r *Redirection, enabled bool) []string {
 		fmt.Sprintf("listenport=%d", r.SrcPort),
 	}
 
-	if enabled == true {
+	if enabled {
 		rule = append(rule, fmt.Sprintf("connectport=%d", r.DstPort))
 		rule = append(rule, fmt.Sprintf("connectaddress=%s", r.DstAddress))
 		rule = append(rule, fmt.Sprintf("protocol=%s", r.Protocol))
@@ -93,7 +93,7 @@ func (f *WindowsFirewall) EnableRedirection(r *Redirection, enabled bool) error 
 	}
 
 	rule := f.generateRule(r, enabled)
-	if enabled == true {
+	if enabled {
 		rule = append([]string{"interface", "portproxy", "add", "v4tov4"}, rule...)
 	} else {
 		rule = append([]string{"interface", "portproxy", "delete", "v4tov4"}, rule...)

main.go
@@ -20,7 +20,7 @@ func main() {
 	defer sess.Close()
 
 	if core.HasColors == false {
-		if *sess.Options.NoColors == true {
+		if *sess.Options.NoColors {
 			fmt.Printf("\n\nWARNING: Terminal colors have been disabled, view will be very limited.\n\n")
 		} else {
 			fmt.Printf("\n\nWARNING: This terminal does not support colors, view will be very limited.\n\n")

modules/ble_options_linux.go
@@ -2,13 +2,15 @@ package modules
 
 import (
 	"github.com/bettercap/gatt"
-	"github.com/bettercap/gatt/linux/cmd"
+	// "github.com/bettercap/gatt/linux/cmd"
 )
 
 var defaultBLEClientOptions = []gatt.Option{
 	gatt.LnxMaxConnections(255),
 	gatt.LnxDeviceID(-1, true),
 }
+
+/*
 
 var defaultBLEServerOptions = []gatt.Option{
 	gatt.LnxMaxConnections(255),
@@ -19,3 +21,5 @@ var defaultBLEServerOptions = []gatt.Option{
 		AdvertisingChannelMap:  0x7,
 	}),
 }
+
+*/

modules/events_stream.go
@@ -157,7 +157,7 @@ func (s *EventsStream) Start() error {
 					s.waitChan <- &e
 				}
 
-				if s.ignoreList.Ignored(e) == false {
+				if !s.ignoreList.Ignored(e) {
 					s.View(e, true)
 				} else {
 					log.Debug("Skipping ignored event %v", e)

modules/http_proxy.go
@@ -72,7 +72,7 @@ func (p *HttpProxy) Configure() error {
 	var scriptPath string
 	var stripSSL bool
 
-	if p.Running() == true {
+	if p.Running() {
 		return session.ErrAlreadyStarted
 	} else if err, address = p.StringParam("http.proxy.address"); err != nil {
 		return err

commit d5b550db28851f6268233eb5fbb88689980fc87d

Fix wifi.ap functionality
Broken in 0de6f3a76eb0155ed9971abc60974e4199361705

modules/wifi_ap.go
@@ -32,7 +32,7 @@ func (w *WiFiModule) parseApConfig() (err error) {
 
 func (w *WiFiModule) startAp() error {
 	// we need channel hopping and packet injection for this
-	if w.Running() {
+	if !w.Running() {
 		return errNoRecon
 	} else if w.apRunning {
 		return session.ErrAlreadyStarted