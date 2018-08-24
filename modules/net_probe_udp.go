031f2e64ff1a8927b9afc3dd7d0a2d583e4146be
evilsocket <evilsocket@gmail.com>
2018-08-24 15:28:33 +0200

    fix: net.probe handled errors were turned into debug messages

--- modules/net_probe_udp.go
+++ modules/net_probe_udp.go
@@ -10,11 +10,12 @@ import (
 func (p *Prober) sendProbeUDP(from net.IP, from_hw net.HardwareAddr, ip net.IP) {
 	name := fmt.Sprintf("%s:137", ip)
 	if addr, err := net.ResolveUDPAddr("udp", name); err != nil {
-		log.Error("Could not resolve %s.", name)
+		log.Debug("could not resolve %s.", name)
 	} else if con, err := net.DialUDP("udp", nil, addr); err != nil {
-		log.Error("Could not dial %s.", name)
+		log.Debug("could not dial %s.", name)
 	} else {
-		// log.Debug("UDP connection to %s enstablished.", name)
+		log.Debug("udp connection to %s enstablished.", name)
+
 		defer con.Close()
 		wrote, _ := con.Write([]byte{0x00})
 
