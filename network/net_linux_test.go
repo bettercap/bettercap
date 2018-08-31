8778a2e9d6bb3d058f857381550419dbd369c511
eenblam <ben.a.elam@gmail.com>
2018-08-31 14:07:47 -0700

    Add iwlist <iface> freq test case

--- network/net_linux_test.go
+++ network/net_linux_test.go
@@ -16,8 +16,27 @@ func TestProcessSupportedFrequencies(t *testing.T) {
 		ExpectedError bool
 	}{
 		{
+			"Returns appropriately formatted frequencies on valid input",
+			`wlan1     11 channels in total; available frequencies :
+			Channel 01 : 2.412 GHz
+			Channel 02 : 2.417 GHz
+			Channel 03 : 2.422 GHz
+			Channel 04 : 2.427 GHz
+			Channel 05 : 2.432 GHz
+			Channel 06 : 2.437 GHz
+			Channel 07 : 2.442 GHz
+			Channel 08 : 2.447 GHz
+			Channel 09 : 2.452 GHz
+			Channel 10 : 2.457 GHz
+			Channel 11 : 2.462 GHz
+			Current Frequency:2.437 GHz (Channel 6)`,
+			nil,
+			[]int{2412, 2417, 2422, 2427, 2432, 2437, 2442, 2447, 2452, 2457, 2462},
+			false,
+		},
+		{
 			"Returns empty with an error",
-			"Shouldn't matter",
+			"Doesn't matter",
 			errors.New("iwlist must have failed"),
 			[]int{},
 			true,
