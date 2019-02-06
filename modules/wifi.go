0a02cee61520f6ccfc090be0979faf245b42f216
evilsocket <evilsocket@gmail.com>
2019-02-06 10:02:18 +0100

    new: wifi.deauth now supports '*' and 'all' as shortcuts for 'ff:ff:ff:ff:ff:ff'

--- modules/wifi.go
+++ modules/wifi.go
@@ -97,9 +97,12 @@ func NewWiFiModule(s *session.Session) *WiFiModule {
 			return err
 		}))
 
-	w.AddHandler(session.NewModuleHandler("wifi.deauth BSSID", `wifi\.deauth ((?:[0-9A-Fa-f]{2}[:-]){5}(?:[0-9A-Fa-f]{2}))`,
-		"Start a 802.11 deauth attack, if an access point BSSID is provided, every client will be deauthenticated, otherwise only the selected client. Use a broadcast BSSID (ff:ff:ff:ff:ff:ff) to iterate every access point with at least one client and start a deauth attack for each one.",
+	w.AddHandler(session.NewModuleHandler("wifi.deauth BSSID", `wifi\.deauth ((?:[a-fA-F0-9:]{11,})|all|\*)`,
+		"Start a 802.11 deauth attack, if an access point BSSID is provided, every client will be deauthenticated, otherwise only the selected client. Use 'all', '*' or a broadcast BSSID (ff:ff:ff:ff:ff:ff) to iterate every access point with at least one client and start a deauth attack for each one.",
 		func(args []string) error {
+			if args[0] == "all" || args[0] == "*" {
+				args[0] = "ff:ff:ff:ff:ff:ff"
+			}
 			bssid, err := net.ParseMAC(args[0])
 			if err != nil {
 				return err
