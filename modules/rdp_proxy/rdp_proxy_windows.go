f00ad8664699166dedb35d4efedeb172024a0631
Alexandre Beaulieu <alex@segfault.me>
2019-04-16 15:26:07 -0400

    feat(RDP): RDP-Proxy prototype.

---
+++ modules/rdp_proxy/rdp_proxy_windows.go
@@ -0,0 +1,39 @@
+package rdp_proxy
+
+import (
+	"github.com/bettercap/bettercap/session"
+)
+
+type RdpProxy struct {
+	session.SessionModule
+}
+
+func NewRdpProxy(s *session.Session) *RdpProxy {
+	return &RdpProxy{
+		SessionModule: session.NewSessionModule("rdp.proxy", s),
+	}
+}
+
+func (mod RdpProxy) Name() string {
+	return "rdp.proxy"
+}
+
+func (mod RdpProxy) Description() string {
+	return "Not supported on this OS"
+}
+
+func (mod RdpProxy) Author() string {
+	return "Alexandre Beaulieu <alex@segfault.me>"
+}
+
+func (mod *RdpProxy) Configure() (err error) {
+	return session.ErrNotSupported
+}
+
+func (mod *RdpProxy) Start() error {
+	return session.ErrNotSupported
+}
+
+func (mod *RdpProxy) Stop() error {
+	return session.ErrNotSupported
+}
