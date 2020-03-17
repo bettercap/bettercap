bcab45be63d6a0666517e57d210f6c1ac56413d9
456vv <123@456vv.com>
2020-03-17 22:06:10 +0800

    Update firewall_windows.go

--- firewall/firewall_windows.go
+++ firewall/firewall_windows.go
@@ -86,7 +86,7 @@ func (f *WindowsFirewall) AllowPort(port int, address string, proto string, allo
 }
 
 func (f *WindowsFirewall) EnableRedirection(r *Redirection, enabled bool) error {
-	if err := f.AllowPort(r.SrcPort, r.DstAddress, r.Protocol, enabled); err != nil {
+	if err := f.AllowPort(r.SrcPort, r.SrcAddress, r.Protocol, enabled); err != nil {
 		return err
 	} else if err := f.AllowPort(r.DstPort, r.DstAddress, r.Protocol, enabled); err != nil {
 		return err
