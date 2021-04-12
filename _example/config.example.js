eb384d67c1e1755bdbb5eb8e62987b471ac8e45d
Simone Margaritelli <evilsocket@gmail.com>
2021-04-12 14:36:27 +0200

    misc: moved example script in dedicated repo

--- _example/config.example.js
+++
@@ -1,3 +0,0 @@
-var wifiInterface = 'put the wifi interface to put in monitor mode here';
-var telegramToken = 'put your telegram bot token here';
-var telegramChatId = 'put your telegram chat id here';
