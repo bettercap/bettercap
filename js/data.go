1d7a49a952605c790ffb43b017fadc14235a1279
☸️ <29265684+buffermet@users.noreply.github.com>
2024-12-06 17:09:27 +0100

    misc

--- js/data.go
+++ js/data.go
@@ -85,7 +85,7 @@ func atob(call otto.FunctionCall) otto.Value {
 
 	arg := argv[0]
 	if (!arg.IsString()) {
-		return ReportError("btoa: single argument must be a string.")
+		return ReportError("atob: single argument must be a string.")
 	}
 
 	decoded, err := base64.StdEncoding.DecodeString(arg.String())
